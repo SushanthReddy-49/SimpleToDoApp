33c61764cf238fc36a34e2e647deaba9bf719dad
SushanthReddy <s.sushanth.reddy49@gmail.com>
2021-12-22 14:05:02 +0530

    Added a delete icon to task

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # I created this (very) simple ToDo App as a practise/Assignment while completing the React Course from Udemy.
+
+Enter the Goal/Task in the input and press Add Goal.
+Use delete option to delete the Task.
--- src/Components/TaskBoard/Task.js
+++ src/Components/TaskBoard/Task.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import './Task.css'
+import React from "react";
+import "./Task.css";
+import { FaTrash } from "react-icons/fa";
 
 const Task = (props) => {
+  const deleteHandler = () => {
+    props.delHandler(props.id);
+  };
 
-    const deleteHandler = () =>{
-        props.delHandler(props.id)
-    }
+  if (props.content === "") {
+    return null;
+  }
+  return (
+    <li className="task-item">
+      {props.content}
+      <div className="delete">
+        <FaTrash className="delete-button" onClick={deleteHandler} />
+      </div>
+    </li>
+  );
+};
 
-    if (props.content===""){
-        return null
-    }
-    return (
-        <li className='task-item' onDoubleClick={deleteHandler}>
-            {props.content}
-        </li>
-    )
-}
-
-export default Task
+export default Task;
